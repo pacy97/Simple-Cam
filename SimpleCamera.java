c5cce91183e7989df06121eb2fcaeb3ae2965c55
Pacy <40542052+pacy97@users.noreply.github.com>
2018-07-08 14:08:40 +0530

    Update SimpleCamera.java

--- SimpleCamera.java
+++ SimpleCamera.java
@@ -1,28 +1,36 @@
 
-package com.example.Pacasian.pacy;
-
-import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-/**
- * Created by sumith on 16-10-2017.
- */
-
-public class about extends AppCompatActivity {
-
-private Button button;
-
+public class SimpleCamera extends AppCompatActivity {
+Button btnc;
+ImageView imv;
+private static final int CAMERA_REQUEST=123;
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.about);
-        button=(TextView)findViewById(R.id.btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               //Add your operation here.......
-            }
-        });
+        setContentView(R.layout.activity_simple_camera);
+        imv=(ImageView)findViewById(R.id.imv);
+        btnc=(Button)findViewById(R.id.btn);
+        
+    }
+
+    public void btnclick(View view) {
+
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,CAMERA_REQUEST);
+    }
+    public  void onActivityResult(int requestCode,int resultCode,Intent data){
+        if (requestCode==CAMERA_REQUEST &&resultCode== Activity.RESULT_OK){
+            Bitmap photo =(Bitmap)data.getExtras().get("data");
+            imv.setImageBitmap(photo);
+        }
     }
 }
